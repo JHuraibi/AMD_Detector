6fa482e09416bd6ff14f0ead50a25f9c228a6576
Tesneem Algahim <fx0285@wayne.edu>
2020-09-28 12:28:34 -0400

    Created test for second eye

--- public/growingCircles/growingCircle.js
+++ public/growingCircles/growingCircle.js
@@ -1,5 +1,9 @@
 var canvas = document.querySelector('canvas');
+var canvas2 = document.getElementById("canvas2");
+var LER = document.getElementById('leftEyeResults');
+var RER = document.getElementById('rightEyeResults');
 var c = canvas.getContext('2d');
+var c2 = canvas2.getContext('2d');
 document.getElementById("button").addEventListener("click", myFunction);
 document.getElementById("start").addEventListener("click", startTest);
 var seen = false;
@@ -7,18 +11,29 @@ var seen = false;
 var size = 500;
 canvas.style.width = size + "px";
 canvas.style.height = size + "px";
+canvas2.style.width = size + "px";
+canvas2.style.height = size + "px";
 
 // Set actual size in memory (scaled to account for extra pixel density).
 var scale = window.devicePixelRatio; // <--- Change to 1 on retina screens to see blurry canvas.
 canvas.width = size * scale;
 canvas.height = size * scale;
+canvas2.width = size * scale;
+canvas2.height = size * scale;
 
 // Normalize coordinate system to use css pixels.
 c.scale(scale, scale);
+c2.scale(scale, scale);
+
+//hide canvas 2 for now
+canvas2.style.display = "none";
+
+
+LER.style.display ="none";
+RER.style.display="none";
 
 
 //Test variables 
-var count = 0;
 var x;
 var y;
 var z = 0;
@@ -33,6 +48,7 @@ c.beginPath();
 c.arc(250, 250, 4, 0, Math.PI * 2, false);
 c.fill();
 c.stroke();
+
 
 function test() {
 
@@ -79,8 +95,8 @@ function test() {
             }
         }
     } else {
-        console.log("Test is over.")
-        showResults(results);
+        console.log("Test is over. Start next eye test")
+        nextTest();
     }
 
 }
@@ -91,19 +107,121 @@ function myFunction() {
 
 function startTest() {                               //start test and create an array for this test
 
-    results = [];
-    results[0] = [];
-    results[1] = [];
-    results[2] = [];
-    index = 0;
-    j = 0;
+    if (canvas2.style.display == "none") {
+        results = [];
+        results[0] = [];
+        results[1] = [];
+        results[2] = [];
+        index = 0;
+        j = 0;
+        seen = false;
+        test();
+    } else {
+        startTest2();
+    }
+}
+
+function nextTest() {
+    canvas.style.display = "none";
+    canvas2.style.display = "inline-block";
+    startTest2();
+
+}
+
+//test2
+//Test variables 
+var x2;
+var y2;
+var z2 = 0;
+var j2 = 0;
+var results2 = [];
+results2[0] = [];
+results2[1] = [];
+results2[2] = [];
+var index2 = 0;
+c2.fillStyle = "black";
+c2.beginPath();
+c2.arc(250, 250, 4, 0, Math.PI * 2, false);
+c2.fill();
+c2.stroke();
+
+
+function test2() {
+
+    if (j2 < 20) {
+        // New red dot location
+        x2 = Math.random() * 500;
+        y2 = Math.random() * 500;
+        z2 = 4
+        c2.clearRect(0, 0, 500, 500)
+
+        // Black Dot
+        c2.fillStyle = "black";
+        c2.beginPath();
+        c2.arc(250, 250, 4, 0, Math.PI * 2, false);
+        c2.fill();
+        c2.stroke();
+        grow2();
+
+
+        // Grow dot
+        function grow2() {
+            c2.fillStyle = "red";
+            c2.beginPath();
+            c2.arc(x2, y2, z2, 0, Math.PI * 2, false);
+            c2.fill();
+            c2.stroke();
+            if (seen == false) {                          //user hasnt seen the red dot yet, keep growing
+                z2++;
+                setTimeout(grow2, 1000);
+            } else if (seen == true && z2 > 5) {                //user seen red dot and it has grown in size- add it to array
+                console.log("create array of the coorinates and size of dot")
+                results2[0][index2] = x2;
+                results2[1][index2] = y2;
+                results2[2][index2] = z2;
+                index2++;
+                j2++;
+                seen = false;
+                test2();
+            }
+            else {                                       //user seen the dot right away
+                j2++;
+                seen = false;
+                test2();
+            }
+        }
+    } else {
+        console.log("Test is over.")
+        showResults(results, results2);
+    }
+
+}
+
+function myFunction() {
+    seen = true;
+}
+
+function startTest2() {                               //start test and create an array for this test
+
+    results2 = [];
+    results2[0] = [];
+    results2[1] = [];
+    results2[2] = [];
+    index2 = 0;
+    j2 = 0;
     seen = false;
-    test();
+    test2();
 }
 
 //Clear the canvas and create a new one with the data collected. Need to also save the data to the database here
 
-function showResults(r) {
+function showResults(r, r2) {
+
+    console.log("displaying results from both tests");
+    canvas2.style.display = "inline-block";
+    canvas.style.display = "inline-block";
+    LER.style.display ="block";
+    RER.style.display="block";
 
     //clear canvas
     c.clearRect(0, 0, 500, 500)
@@ -114,6 +232,15 @@ function showResults(r) {
     c.arc(250, 250, 4, 0, Math.PI * 2, false);
     c.fill();
     c.stroke();
+
+    c2.clearRect(0, 0, 500, 500)
+
+    // Black Dot
+    c2.fillStyle = "black";
+    c2.beginPath();
+    c2.arc(250, 250, 4, 0, Math.PI * 2, false);
+    c2.fill();
+    c2.stroke();
 
     //plot all the circles the user missed if any
     var limit = r[0].length;
@@ -130,4 +257,20 @@ function showResults(r) {
         }
     }
 
+    var limit2 = r2[0].length;
+    if (limit2 > 0) {
+        for (var i = 0; i < limit; i++) {
+            x2 = r2[0][i];
+            y2 = r2[1][i];
+            z2 = r2[2][i];
+            c2.fillStyle = "red";
+            c2.beginPath();
+            c2.arc(x2, y2, z2, 0, Math.PI * 2, false);
+            c2.fill();
+            c2.stroke();
+        }
+    }
+
 }
+
+
