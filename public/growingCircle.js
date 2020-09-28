149bb88d513290dcd64eb3f63dfdb56fe71fa004
Tesneem Algahim <fx0285@wayne.edu>
2020-09-28 10:01:45 -0400

    Instructions & cleanup

--- public/growingCircle.js
+++ public/growingCircle.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('canvas');
 var c = canvas.getContext('2d');
 document.getElementById("button").addEventListener("click", myFunction);
 document.getElementById("start").addEventListener("click", startTest);
-var seen= false;
+var seen = false;
 
 var size = 500;
 canvas.style.width = size + "px";
@@ -18,114 +18,108 @@ c.scale(scale, scale);
 
 
 //Test variables 
-var count=0;
+var count = 0;
 var x;
 var y;
-var z=0;
-var j=0;
+var z = 0;
+var j = 0;
 var results = [];
-results[0] =[];
-results[1] =[];
-results[2] =[];
-var index=0;
+results[0] = [];
+results[1] = [];
+results[2] = [];
+var index = 0;
 
-function test(){
+function test() {
 
-    if(j<20){   
-    // New red dot location
-    x = Math.random() * 500;
-    y = Math.random() * 500;
-    z=4
-    c.clearRect(0,0, 500, 500)
+    if (j < 20) {
+        // New red dot location
+        x = Math.random() * 500;
+        y = Math.random() * 500;
+        z = 4
+        c.clearRect(0, 0, 500, 500)
 
-    // Black Dot
-    c.fillStyle="black";
-    c.beginPath();
-    c.arc(250, 250, 4, 0, Math.PI*2, false);
-    c.fill();
-    c.stroke();
-    grow();
-    
-
-    // Grow dot
-    function grow(){
-        c.fillStyle="red";
+        // Black Dot
+        c.fillStyle = "black";
         c.beginPath();
-        c.arc(x, y, z, 0, Math.PI*2, false);
+        c.arc(250, 250, 4, 0, Math.PI * 2, false);
         c.fill();
         c.stroke();
-        if (seen==false) {                          //user hasnt seen the red dot yet, keep growing
-            z++;
-            setTimeout(grow, 1000);
-        }else if(seen==true && z>5){                //user seen red dot and it has grown in size- add it to array
-            console.log("create array of the coorinates and size of dot")
-            results[0][index]=x;
-            results[1][index]=y;
-            results[2][index]=z;
-            index++;
-            j++;
-            seen=false;
-            test();
-        }
-        else{                                       //user seen the dot right away
-            j++;
-            seen=false;
-            test(); 
+        grow();
+
+
+        // Grow dot
+        function grow() {
+            c.fillStyle = "red";
+            c.beginPath();
+            c.arc(x, y, z, 0, Math.PI * 2, false);
+            c.fill();
+            c.stroke();
+            if (seen == false) {                          //user hasnt seen the red dot yet, keep growing
+                z++;
+                setTimeout(grow, 1000);
+            } else if (seen == true && z > 5) {                //user seen red dot and it has grown in size- add it to array
+                console.log("create array of the coorinates and size of dot")
+                results[0][index] = x;
+                results[1][index] = y;
+                results[2][index] = z;
+                index++;
+                j++;
+                seen = false;
+                test();
+            }
+            else {                                       //user seen the dot right away
+                j++;
+                seen = false;
+                test();
             }
         }
-    }else{
-        console.log("Test is over.") 
-        showResults(results); 
-    } 
+    } else {
+        console.log("Test is over.")
+        showResults(results);
+    }
 
 }
 
-function myFunction(){
-    seen=true;
+function myFunction() {
+    seen = true;
 }
 
-function startTest(){                               //start test and create an array for this test
+function startTest() {                               //start test and create an array for this test
+
     results = [];
-    results[0] =[];
-    results[1] =[];
-    results[2] =[];
-    index=0;
-    j=0;
-    seen=false;
+    results[0] = [];
+    results[1] = [];
+    results[2] = [];
+    index = 0;
+    j = 0;
+    seen = false;
     test();
 }
 
-function showResults(r){                        //Clear the canvas and create a new one with the data collected
+function showResults(r) {                        //Clear the canvas and create a new one with the data collected
     //clear canvas
-    c.clearRect(0,0, 500, 500)
+    c.clearRect(0, 0, 500, 500)
 
     // Black Dot
-    c.fillStyle="black";
+    c.fillStyle = "black";
     c.beginPath();
-    c.arc(250, 250, 4, 0, Math.PI*2, false);
+    c.arc(250, 250, 4, 0, Math.PI * 2, false);
     c.fill();
     c.stroke();
 
     //plot all the circles the user missed if any
-    var limit=r[0].length;
-    if(limit>0){
-        for(var i=0;i<limit;i++){
-            x=r[0][i];
-            y=r[1][i];
-            z=r[2][i];
-            c.fillStyle="red";
+    var limit = r[0].length;
+    if (limit > 0) {
+        for (var i = 0; i < limit; i++) {
+            x = r[0][i];
+            y = r[1][i];
+            z = r[2][i];
+            c.fillStyle = "red";
             c.beginPath();
-            c.arc(x, y, z, 0, Math.PI*2, false);
+            c.arc(x, y, z, 0, Math.PI * 2, false);
             c.fill();
             c.stroke();
         }
     }
-    
+
 }
-
-
-
-
-
-
-
