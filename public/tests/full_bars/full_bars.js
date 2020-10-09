5cb5042890216e9a99a8ea08da870c8d5bdd1fd1
JHuraibi <fh1328@wayne.edu>
2020-10-09 00:55:25 -0400

    Added standard grid, borders, and center dot to "Full Bars"

--- public/tests/full_bars/full_bars.js
+++ public/tests/full_bars/full_bars.js
@@ -1,82 +1,120 @@
 let numBars = 4;
 let queue = [];
 let timer = 0;
-let barW = 20;
-let current = 0;
-let sec = 60;
-let xLocations = [];
-let yLocations = [];
+let xClickLocations = [];
+let yClickLocations = [];
 let clickPos = 0;
 
-let endGame = false;
-let xDone = false;
+let gameFinished = false;
+let xBarsDone = false;
+
+let sec = 2;					// Seconds between showing each bar
+let currentPos = 0;				// X or Y value to draw the next bar at
 
 function setup() {
-	createCanvas(1000, 1000);
+	createCanvas(800, 800);
 	background(150);
 	initializeQueue();
 }
 
+// Would a router switch statement be easier to read?
 function draw() {
-	if (!endGame) {
-		background(150);
-		timer++;
-		drawBar();
+	background(150);
 
-		if (timer % (1.5 * sec) == 0) {
-			nextBar();
+	drawStaticGrid();				// Draw first so other items can be drawn on top of it
+
+	if (!gameFinished) {
+
+		if (timer % (60 * sec) === 0) {
+			loadNextBar();
 		}
+
+		drawBar();
+		timer++;
 	}
 	else {
-		background(240);
-		drawAllBars();
+		drawClickedBars();
 	}
+
+	drawCenterDot();				// Draw center dot last so it is always visible (i.e. nothing drawn over it)
+	drawStaticBorder();				// ^^ Same with the border.
 }
 
 function mousePressed() {
-	if (!xDone) {
-		if (xLocations[clickPos] != current) {
-			xLocations[clickPos] = current;
+	if (!xBarsDone) {
+		if (xClickLocations[clickPos] !== currentPos) {
+			xClickLocations[clickPos] = currentPos;
 			clickPos++;
 		}
 	}
 	else {
-		if (yLocations[clickPos] != current) {
-			yLocations[clickPos] = current;
+		if (yClickLocations[clickPos] !== currentPos) {
+			yClickLocations[clickPos] = currentPos;
 			clickPos++;
 		}
 	}
+}
+
+function drawStaticBorder() {
+	noFill();
+	strokeWeight(10);
+	stroke(0);
+	rect(0, 0, width, height);
+}
+
+function drawStaticGrid() {
+	fill(0);
+	noStroke();
+
+	var interval = width / 20;
+
+	// Vertical grid lines
+	for (let i = 0; i < 20; i++) {
+		rect((i * interval), 0, 1, height);
+	}
+
+	// Horizontal grid lines
+	for (let i = 0; i < 20; i++) {
+		rect(0, (i * interval), width, 1);
+	}
+}
+
+function drawCenterDot() {
+	fill(0);
+	strokeWeight(2);
+	stroke(255);
+	ellipse(width / 2, height / 2, 20);
 }
 
 function drawBar() {
-	let c = barW;
+	fill(0);
+
+	let barW = width / 20;
 	noStroke();
 
-	if (!xDone) {
-		fill(70, 0, 0);
-		rect(current, 0, barW, height);
+	if (!xBarsDone) {
+		rect(currentPos, 0, barW, height);
 	}
 	else {
-		fill(0, 0, 70);
-		rect(0, current, width, barW);
+		rect(0, currentPos, width, barW);
 	}
 }
 
-function drawAllBars() {
-	let numClickedX = xLocations.length;
-	let numClickedY = yLocations.length;
+function drawClickedBars() {
+	let numClickedX = xClickLocations.length;
+	let numClickedY = yClickLocations.length;
 
-	let c = barW;
+	let barW = width / 20;
 
 	for (let i = 0; i < numClickedX; i++) {
-		let x = xLocations[i];
+		let x = xClickLocations[i];
 		noStroke();
 		fill(70, 0, 70, 50);
 		rect(x, 0, barW, height);
 	}
 
 	for (let i = 0; i < numClickedY; i++) {
-		let y = yLocations[i];
+		let y = yClickLocations[i];
 		noStroke();
 		fill(70, 0, 70, 50);
 		rect(0, y, width, barW);
@@ -84,6 +122,8 @@ function drawAllBars() {
 }
 
 function initializeQueue() {
+	console.log("Queue Empty. Repopulating.");
+
 	for (let i = 0; i < numBars; i++) {
 		queue[i] = (width / numBars) * i;
 	}
@@ -91,18 +131,22 @@ function initializeQueue() {
 	queue = shuffle(queue);
 }
 
-function nextBar() {
-	if (queue.length <= 0 && xDone == false) {
-		console.log("Queue Empty");
-		clickPos = 0;
-		initializeQueue();
-		xDone = true;
-		console.log("X DONE");
+function loadNextBar() {
+	if (queue.length <= 0) {
+		if (xBarsDone === false) {
+			initializeQueue();
+			clickPos = 0;
+			xBarsDone = true;
+
+			console.log("Vertical Bars (x) DONE");
+		}
 	}
-	else if (queue.length <= 0 && xDone == true) {
-		endGame = true;
-		console.log("Y DONE");
+	else if (queue.length <= 0) {
+		if (xBarsDone === true) {
+			console.log("Horizontal Bars (Y) DONE");
+			gameFinished = true;
+		}
 	}
 
-	current = queue.pop();
+	currentPos = queue.pop();
 }
