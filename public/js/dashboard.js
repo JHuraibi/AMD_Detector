baf65f31ff579304ecb08d7ef13639c887d0b74a
JHuraibi <fh1328@wayne.edu>
2020-10-18 14:43:43 -0400

    Added function to populate the canvases in the grid

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,59 +1,63 @@
+// Look into transform (e.g. skewX). Possibly able to draw the chart normally then just scale it down.
+
 /* globals Chart:false, feather:false */
 
 (function () {
-  'use strict'
+	'use strict'
 
-  feather.replace()
+	feather.replace()
 
-  // Graphs
-  var ctx = document.getElementById('myChart')
-  // eslint-disable-next-line no-unused-vars
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: [
-        'January',
-        'Februrary',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October (Current)'
-      ],
-      datasets: [{
-        data: [
-          202,
-          189,
-          163,
-          175,
-          144,
-          60,
-          45,
-          42,
-          37,
-          20
-        ],
-        lineTension: 0,
-        backgroundColor: 'transparent',
-        borderColor: '#007bff',
-        borderWidth: 4,
-        pointBackgroundColor: '#007bff'
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
-          }
-        }]
-      },
-      legend: {
-        display: false
-      }
-    }
-  })
+	// Graphs
+	var ctx = document.getElementById('myChart')
+	// eslint-disable-next-line no-unused-vars
+	var myChart = new Chart(ctx, {
+		type: 'line',
+		data: {
+			labels: [
+				'January', 'Februrary', 'March', 'April',
+				'May', 'June', 'July', 'August',
+				'September', 'October (Current)'
+			],
+			datasets: [{
+				data: [
+					202, 189, 163, 175, 144, 60, 45, 42, 37, 20
+				],
+				lineTension: 0,
+				backgroundColor: 'transparent',
+				borderColor: '#007bff',
+				borderWidth: 4,
+				pointBackgroundColor: '#007bff'
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: false
+					}
+				}]
+			},
+			legend: {
+				display: false
+			}
+		}
+	})
 }())
+
+// fillRect(x, y, width, height)
+function drawFullBars(chartID, parentSize) {
+	let canvas = document.getElementById(chartID);
+	let context = canvas.getContext('2d');
+
+	let barW = 2;
+
+	let xLocations = [25, 100];
+	let yLocations = [50, 150];
+
+	for (let i = 0; i < xLocations.length; i++) {
+		let xPos = xLocations[i];
+		context.fillRect(xPos, 0, barW, parentSize);
+		// console.log("X: " + xPos);
+
+	}
+}
