0d29a9b3efe4e6f1d63c4585770f97098413bfd9
er0531 <er0531@wayne.edu>
2020-11-07 16:51:12 -0500

    Fixed the Amsler grid to match recent branch

--- public/tests/AmslerGrid/Amsler.js
+++ public/tests/AmslerGrid/Amsler.js
@@ -1,39 +1,65 @@
-var sd = document.getElementById("strtdiv");
-var Ampic = document.getElementById("amslerpic");
-var next = document.getElementById("nxt");
-var start = document.getElementById("strt");
-var time = document.getElementById("timer");
-next.style.display = "none";
+var sec1Questions = document.getElementById("qSec1");
+var sec2Questions = document.getElementById("qSec2");
+var sec3Questions = document.getElementById("qSec3");
+var sec4Questions = document.getElementById("qSec4");
+document.getElementById("sp1").addEventListener("click", s1);
+document.getElementById("sp2").addEventListener("click", s2);
+document.getElementById("sp3").addEventListener("click", s3);
+document.getElementById("sp4").addEventListener("click", s4);
 
-// Ampic.style.display = "none"
 
-start.style.display = "inline-block";
 
-var countDown = 10;
-document.getElementById("timer").innerHTML =  countDown +" seconds"; 
-function interval()
-{
-    
-    start.style.display = "none";
-    Ampic.style.display = "inline-block"
-    
-    document.getElementById("timer").innerHTML = countDown +" seconds"; 
-    if(countDown != 0)
-    {
-        setTimeout(interval, 1000)
-        countDown--;
+sec1Questions.style.display = "none"
+sec2Questions.style.display = "none"
+sec3Questions.style.display = "none"
+sec4Questions.style.display = "none"
 
+
+
+
+function s1() {
+
+
+    if (sec1Questions.style.display === "none") {
+        sec1Questions.style.display = "inline-block";
+    } else {
+        sec1Questions.style.display = "none";
     }
-    else {
-        displayNext();
+}
+
+function s2() {
+
+
+    if (sec2Questions.style.display === "none") {
+        sec2Questions.style.display = "inline-block";
+    } else {
+        sec2Questions.style.display = "none";
+    }
+}
+
+function s3() {
+
+    if (sec3Questions.style.display === "none") {
+        sec3Questions.style.display = "inline-block";
+    } else {
+        sec3Questions.style.display = "none";
     }
 
 }
 
+function s4() {
+    if (sec4Questions.style.display === "none") {
+        sec4Questions.style.display = "inline-block";
+    } else {
+        sec4Questions.style.display = "none";
+    }
 
-function displayNext() {
-    next.style.display = "block";
+
 }
+
+
+
+
 
 
 
