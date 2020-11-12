9f04c9de45c150afa21ead3568431db9a85650af
Tesneem Algahim <fx0285@wayne.edu>
2020-11-12 13:36:43 -0500

    Sign-out button - Physicians side
    Sign-out buttons functional on physician pages. Minor changes to homepage to accommodate fractal test.

--- public/patients/myPhysicians.js
+++ public/patients/myPhysicians.js
@@ -302,3 +302,16 @@ async function deletePhysician(docID) {
 
 }
 
+const signOut = document.querySelector('.sign-out');
+
+// sign out
+signOut.addEventListener('click', () => {
+  firebase.auth().signOut()
+    .then(() => console.log('signed out'));
+    var url=location.href.split("/").slice(-1) + "";
+    if(url.startsWith("instructions_page.html", 0)){
+      window.location = '../index.html';
+    }else
+      window.location = '../index.html'; 
+});
+
