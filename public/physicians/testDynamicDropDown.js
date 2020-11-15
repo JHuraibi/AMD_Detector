7d4e41ba1fb4de57f4c9616ccb1521579ea30b09
Tesneem Algahim <fx0285@wayne.edu>
2020-11-15 15:00:15 -0500

    Dynamic drop down menu functional
    drop down menu in physicians dashboard loads the physicians current patient list and their data

--- public/physicians/testDynamicDropDown.js
+++ public/physicians/testDynamicDropDown.js
@@ -2,12 +2,39 @@ let dropDown = document.getElementById("patientList");
 let patients = [];
 
 (function () {
-	let patient1 = {
+
+	firebase.auth().onAuthStateChanged(user => {
+		if (user) {
+			let id = user.uid;
+			db.collection("users").doc(id)
+				.get()
+				.then(doc => {
+					let array = doc.data().patients;
+
+					for (var i = 0; i < array.length; i++) {
+						db.collection("users").doc(array[i])
+							.get()
+							.then(doc => {
+								let patient3 = {
+									id: doc.id,
+									firstName: doc.data().firstname,
+									lastName: doc.data().lastname,
+								}
+								patients.push(patient3);
+								console.log("Pushing patient " + i);
+								dropdown();
+							});
+					}
+				});
+		}
+	});
+
+/* 	let patient1 = {
 		id: "Ii4NKqGowiVtH14FK8O3b7Wfuri1",
 		firstName: "John",
 		lastName: "Doe",
 	}
-	
+
 	let patient2 = {
 		id: "NqP2JTgoCpYPixkCFVkvFyHUs7x1",
 		firstName: "Smitty",
@@ -15,25 +42,25 @@ let patients = [];
 	}
 	console.log("PUSHING USERS");
 	patients.push(patient1);
-	patients.push(patient2);
+	patients.push(patient2); */
 }());
 
 
-(function () {
+function dropdown() {
 	console.log("PUSHING TO DROPDOWN");
-	
+
 	for (let i = 0; i < patients.length; i++) {
 		let p = patients[i];
 		let a = document.createElement("a");
 		let patientName = document.createTextNode(p.firstName + " " + p.lastName);
-		
+
 		a.appendChild(patientName);
 		a.setAttribute("href", URIBuilder(p));
-		
+
 		dropDown.appendChild(a);
 	}
-	
-}());
+
+}
 
 (function () {
 
