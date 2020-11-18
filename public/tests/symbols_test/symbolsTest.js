8f228118a5bd2e77aac31f0776721e43eb26f6d2
er0531 <er0531@wayne.edu>
2020-11-17 22:40:35 -0500

    Added options to choose which eye to test

--- public/tests/symbols_test/symbolsTest.js
+++ public/tests/symbols_test/symbolsTest.js
@@ -8,16 +8,27 @@
 // //
 
 let timestamp = Date.now();
-var testOneInProgress = true;
 
-var nexteye = document.getElementById('nextEye');
-var strt1 = document.getElementById('start_test1');
-var strt2 = document.getElementById('start_test2');
+
+//Bool values for tests that are in progress
+var rightEyeInProgress;
+var leftEyeInProgress;
+var bothREyeinProgress;
+var bothLEyeInProgress;
+
+//Html elements
 var canvas = document.getElementById('canvas1');
 var canvas2 = document.getElementById("canvas2");
+var rightBtn = document.getElementById("rightBtn");
+var leftBtn = document.getElementById("leftBtn");
+var bothBtn = document.getElementById("bothBtn");
+var nexteyebtn = document.getElementById("nexttestbtn");
+var nextEye = document.getElementById("nextEye");
 var c = canvas.getContext('2d');
 var c2 = canvas2.getContext('2d');
+let exitBtns = document.getElementById('exitTestBtns');
 
+//Canvas Sizing
 var size = 600;
 canvas.style.width = size + "px";
 canvas.style.height = size + "px";
@@ -32,6 +43,8 @@ canvas2.height = size * scale;
 
 c.scale(scale, scale);
 c2.scale(scale, scale);
+
+//Speed of test
 var speed = 3000;
 var id;
 var db = firebase.firestore();
@@ -55,7 +68,7 @@ async function getUid() {
 }
 
 window.addEventListener('keydown', function (e) {
-	
+
 	// var key = e.keyCode;
 
 	// key variable
@@ -94,7 +107,7 @@ c.fillRect(0, 0, 600, 600);
 blackDot();
 
 // Variable
-var x, y, y2, x2;
+var x, y;
 var symbols = ["+", "-", "x", "÷"];
 var rightResultX = [];
 var rightResultY = [];
@@ -105,7 +118,7 @@ var leftResultsSymbols = [];
 
 // Not displaying second canvas
 canvas2.style.display = "none";
-nexteye.style.display = "none";
+
 
 // index to capture result
 var t = 0;
@@ -113,26 +126,50 @@ var t2 = 0;
 
 // if the key pressed is not a capture the result for the next 4 functions
 function aKey() {
-	if (testOneInProgress) {
+	if (rightEyeInProgress) {
 		if (symbols[r] != "+") {
 			rightResultsSymbols[t] = symbols[r];
 			rightResultX[t] = x;
 			rightResultY[t] = y;
 			t++;
+
 		}
 	}
-	else {
+	if (leftEyeInProgress) {
 		if (symbols[r2] != "+") {
 			leftResultsSymbols[t2] = symbols[r2];
 			leftResultX[t2] = x;
 			leftResultY[t2] = y;
 			t2++;
+			console.log("")
 		}
 	}
+	if (bothREyeinProgress) {
+		if (symbols[r] != "+") {
+			rightResultsSymbols[t] = symbols[r];
+			rightResultX[t] = x;
+			rightResultY[t] = y;
+			t++;
+
+		}
+
+	}
+	else if (bothLEyeInProgress) {
+		if (symbols[r2] != "+") {
+			leftResultsSymbols[t2] = symbols[r2];
+			leftResultX[t2] = x;
+			leftResultY[t2] = y;
+			t2++;
+			console.log("")
+		}
+
+
+	}
+
 }
 
 function sKey() {
-	if (testOneInProgress) {
+	if (rightEyeInProgress) {
 		if (symbols[r] != "-") {
 			rightResultsSymbols[t] = symbols[r];
 			rightResultX[t] = x;
@@ -141,22 +178,41 @@ function sKey() {
 
 		}
 	}
-	else {
+	if (leftEyeInProgress) {
 		if (symbols[r2] != "-") {
 			leftResultsSymbols[t2] = symbols[r2];
 			leftResultX[t2] = x;
 			leftResultY[t2] = y;
 			t2++;
+			console.log("")
+		}
+	}
+	if (bothREyeinProgress) {
+		if (symbols[r] != "-") {
+			rightResultsSymbols[t] = symbols[r];
+			rightResultX[t] = x;
+			rightResultY[t] = y;
+			t++;
 
 		}
 
 	}
+	else if (bothLEyeInProgress) {
+		if (symbols[r2] != "-") {
+			leftResultsSymbols[t2] = symbols[r2];
+			leftResultX[t2] = x;
+			leftResultY[t2] = y;
+			t2++;
+			console.log("")
+		}
 
+
+	}
 
 }
 
 function xKey() {
-	if (testOneInProgress) {
+	if (rightEyeInProgress) {
 		if (symbols[r] != "x") {
 			rightResultsSymbols[t] = symbols[r];
 			rightResultX[t] = x;
@@ -165,14 +221,34 @@ function xKey() {
 
 		}
 	}
-	else {
+	if (leftEyeInProgress) {
 		if (symbols[r2] != "x") {
 			leftResultsSymbols[t2] = symbols[r2];
 			leftResultX[t2] = x;
 			leftResultY[t2] = y;
 			t2++;
+			console.log("")
+		}
+	}
+	if (bothREyeinProgress) {
+		if (symbols[r] != "x") {
+			rightResultsSymbols[t] = symbols[r];
+			rightResultX[t] = x;
+			rightResultY[t] = y;
+			t++;
 
 		}
+
+	}
+	else if (bothLEyeInProgress) {
+		if (symbols[r2] != "x") {
+			leftResultsSymbols[t2] = symbols[r2];
+			leftResultX[t2] = x;
+			leftResultY[t2] = y;
+			t2++;
+			console.log("")
+		}
+
 
 	}
 
@@ -180,16 +256,35 @@ function xKey() {
 }
 
 function dKey() {
-	if (testOneInProgress) {
+	if (rightEyeInProgress) {
 		if (symbols[r] != "÷") {
 			rightResultsSymbols[t] = symbols[r];
 			rightResultX[t] = x;
-			righResultY[t] = y;
+			rightResultY[t] = y;
 			t++;
 
 		}
 	}
-	else {
+	if (leftEyeInProgress) {
+		if (symbols[r2] != "÷") {
+			leftResultsSymbols[t2] = symbols[r2];
+			leftResultX[t2] = x;
+			leftResultY[t2] = y;
+			t2++;
+			console.log("")
+		}
+	}
+	if (bothREyeinProgress) {
+		if (symbols[r] != "÷") {
+			rightResultsSymbols[t] = symbols[r];
+			rightResultX[t] = x;
+			rightResultY[t] = y;
+			t++;
+
+		}
+
+	}
+	else if (bothLEyeInProgress) {
 		if (symbols[r2] != "÷") {
 			leftResultsSymbols[t2] = symbols[r2];
 			leftResultX[t2] = x;
@@ -198,8 +293,18 @@ function dKey() {
 			console.log("")
 		}
 
+
 	}
 
+}
+
+// function to hide display buttons
+nexteyebtn.style.display = "none";
+nextEye.style.display = "none";
+function hideBtns() {
+	rightBtn.style.display = "none";
+	leftBtn.style.display = "none";
+	bothBtn.style.display = "none";
 }
 
 
@@ -228,6 +333,7 @@ function blackDot() {
 	c2.fill();
 }
 
+// indexing random symbols
 var r;
 var r2;
 
@@ -235,16 +341,18 @@ var r2;
 function randomSymbol() {
 	r = Math.floor(Math.random() * 4);
 	r2 = Math.floor(Math.random() * 4);
-	x = Math.floor(Math.random() * 600);
-	y = Math.floor(Math.random() * 600);
+	x = Math.floor(Math.random() * 580);
+	y = Math.floor(Math.random() * 580);
+	
 
-	if (testOneInProgress) {
+
+	if (rightEyeInProgress) {
 		c.beginPath();
 		c.fillStyle = "red";
 		c.font = "40px Arial";
 		c.fillText(symbols[r], x, y);
 	}
-	else {
+	if (leftEyeInProgress) {
 		console.log("In the else c2 random symbols")
 		// Canvas 2
 		c2.beginPath();
@@ -252,6 +360,60 @@ function randomSymbol() {
 		c2.font = "40px Arial";
 		c2.fillText(symbols[r2], x, y);
 	}
+	if (bothREyeinProgress) {
+		c.beginPath();
+		c.fillStyle = "red";
+		c.font = "40px Arial";
+		c.fillText(symbols[r], x, y);
+
+	}
+	else if (bothLEyeInProgress) {
+		c2.beginPath();
+		c2.fillStyle = "red";
+		c2.font = "40px Arial";
+		c2.fillText(symbols[r2], x, y);
+
+	}
+
+}
+
+//Right eye option
+function rightOption() {
+	hideBtns();
+	rightEyeInProgress = true;
+	bothREyeinProgress = false;
+	leftEyeInProgress = false;
+	if (rightEyeInProgress) {
+		rightEyeTest();
+
+	}
+
+}
+
+function leftOption() {
+
+	hideBtns();
+	leftEyeInProgress = true;
+	rightEyeInProgress = false;
+	bothREyeinProgress = false;
+	if (leftEyeInProgress) {
+		leftEyeTest();
+	}
+
+
+
+}
+function bothOption() {
+
+	hideBtns();
+	bothREyeinProgress = true;
+	rightEyeInProgress = false;
+	leftEyeInProgress = false;
+	if (bothREyeinProgress) {
+		rightEyeTest();
+	}
+
+
 
 }
 
@@ -260,20 +422,41 @@ var i = 0;
 
 function rightEyeTest() {
 
-	strt1.style.display = "none";
-	console.log("test 1");
-	blackDot();
-	if (i < 5) {
-		clearCanvas();
+
+
+	if (rightEyeInProgress) {
+		console.log("Right eye test");
 		blackDot();
-		randomSymbol();
-		i++;
-		setTimeout(rightEyeTest, speed);
-		// setTimeout(test1, 1000);	// !! FOR TESTING
+		if (i < 5) {
+			clearCanvas();
+			blackDot();
+			randomSymbol();
+			i++;
+			setTimeout(rightEyeTest, speed);
+			// setTimeout(test1, 1000);	// !! FOR TESTING
+			
+		}
+		else {
+
+			rightResults();
+		}
 	}
-	else {
-		console.log("Going to next test");
-		nexttest();
+	if (bothREyeinProgress) {
+		console.log("both eye test");
+		blackDot();
+		if (i < 5) {
+			clearCanvas();
+			blackDot();
+			randomSymbol();
+			i++;
+			setTimeout(rightEyeTest, speed);
+			// setTimeout(test1, 1000);	// !! FOR TESTING
+		}
+		else {
+
+			nexttest();
+		}
+
 	}
 
 }
@@ -283,32 +466,61 @@ function nexttest() {
 	canvas.style.display = "none";
 	canvas2.style.display = "none";
 
-	var startTest1 = document.getElementById("start_test1");
-	var startTest2 = document.getElementById("start_test2");
 
-	startTest1.style.display = "none";
-	nexteye.style.display = "block";
+	nextEye.style.display = "inline-block";
+	nexteyebtn.style.display = "inline-block";
+	bothREyeinProgress = false;
+	bothLEyeInProgress= true;
 
-	testOneInProgress = false;
 }
 
+
+// Representing left eye iterations
 var a = 0;
 
 function leftEyeTest() {
 
+	canvas.style.display = "none";
 	canvas2.style.display = "inline-block";
-	nexteye.style.display = "none";
+
 	// console.log("Test2");
-	if (a < 5) {
-		// console.log("In test 2 loop");
-		clearCanvas();
-		blackDot();
-		randomSymbol();
-		a++;
-		setTimeout(leftEyeTest, speed);
-		// setTimeout(test2, 1000);	// !! FOR TESTING
+	if (leftEyeInProgress) {
+		if (a < 5) {
+			// console.log("In test 2 loop");
+			clearCanvas();
+			blackDot();
+			randomSymbol();
+			a++;
+			setTimeout(leftEyeTest, speed);
+			
+			// setTimeout(test2, 1000);	// !! FOR TESTING
+		}
+		else{
+		leftResults();
+		}
 	}
-	else results();
+
+	if (bothLEyeInProgress) {
+		nextEye.style.display = "none";
+		nexteyebtn.style.display = "none";
+		exitBtns.style.display = "none";
+		if (a < 5) {
+			// console.log("In test 2 loop");
+			clearCanvas();
+			blackDot();
+			randomSymbol();
+			a++;
+			setTimeout(leftEyeTest, speed);
+			// setTimeout(test2, 1000);	// !! FOR TESTING
+		}
+		else
+		{
+			console.log("I'm in the both results else")
+			bothResults();
+		}
+
+
+	}
 }
 
 var j;
@@ -318,7 +530,7 @@ var j2;
 // blue represents right eye
 // orange represents left eye
 // function to show the erros after test is conducted
-function results() {
+function bothResults() {
 
 	// console.log("Result Symbols: " + resultsSymbols);
 
@@ -340,13 +552,51 @@ function results() {
 		c2.fillStyle = "orange";
 		c2.font = "35 px Arial";
 		c2.fillText(leftResultsSymbols[j2], leftResultX[j2], leftResultY[j2]);
-		
+
 	}
 	showExitButton();
 }
 
+//Right Eye Results
+function rightResults() {
+	console.log("Reading from right results");
+	canvas.style.display = "inline-block";
+	canvas2.style.display = "none";
+	clearCanvas();
+	blackDot();
+
+	for (j = 0; j < rightResultsSymbols.length; j++) {
+		c.fillStyle = "blue";
+		c.font = "35 px Arial";
+		c.fillText(rightResultsSymbols[j], rightResultX[j], rightResultY[j]);
+
+	}
+	showExitButton();
+
+
+}
+
+function leftResults() {
+	canvas.style.display = "none";
+	canvas2.style.display = "inline-block";
+	clearCanvas();
+	blackDot();
+	for (j2 = 0; j2 < leftResultsSymbols.length; j2++) {
+		console.log("In loop of c2 results ");
+		c2.fillStyle = "orange";
+		c2.font = "35 px Arial";
+		c2.fillText(leftResultsSymbols[j2], leftResultX[j2], leftResultY[j2]);
+
+	}
+	showExitButton();
+
+
+}
+
+
 function showExitButton() {
-	let exitBtns = document.getElementById('exitTestBtns');
+
+	exitBtns.style.display = "inline-block";
 	let fadeInSpeed = 1;
 
 	exitBtns.style.display = "inherit";
