e96a4199132803efeef202362f175a9352993340
Tesneem Algahim <fx0285@wayne.edu>
2020-11-24 13:30:26 -0500

    Update auth.js

--- public/auth.js
+++ public/auth.js
@@ -14,38 +14,55 @@ authSwitchLinks.forEach(link => {
 registerForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  // get user info
-  const email = registerForm.email.value;
-  const password = registerForm.password.value;
+  //error handling
+  try {
+    if (registerForm['firstname'].value == "") {
+      throw "Error: Enter a First Name";
+    }
+    if (registerForm['lastname'].value == "") {
+      throw "Error: Enter a Last Name";
+    }
+    if (registerForm['birthdate'].value == "") {
+      throw "Error: Enter a Birthdate";
+    }
+    if (registerForm['email'].value == "") {
+      throw "Error: Enter an email";
+    }
+  } catch (error) {
+    registerForm.querySelector('.error').textContent = error;
+    return;
+  }
+ 
+// get user info
+    const email = registerForm.email.value;
+    const password = registerForm.password.value;
+  
+    // sign up the user & add firestore data
+    firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
+      return db.collection('users').doc(cred.user.uid).set({
+        email: registerForm['email'].value,
+        firstname: registerForm['firstname'].value,
+        lastname: registerForm['lastname'].value,
+        birthday: registerForm['birthdate'].value,
+        gender: registerForm['gender'].value,
+        testSpeeds: 1,
+        type: "user"
+      });
+    }).then(() => {
+      firebase.auth().onAuthStateChanged(user => {
+        if (user) {
+          id = user.uid;
+          console.log(id);
+          db.collection("TestResults").doc(id).set({
+            exists: true
+          }).then(() => { sendemail(); });
+        }
+      });
+      //window.location = 'index.html';
+    }).catch(error => {
+      registerForm.querySelector('.error').textContent = error.message;
+    });  
 
-  // sign up the user & add firestore data
-  firebase.auth().createUserWithEmailAndPassword(email, password).then(cred => {
-    return db.collection('users').doc(cred.user.uid).set({
-      email: registerForm['email'].value,
-      firstname: registerForm['firstname'].value,
-      lastname: registerForm['lastname'].value,
-      birthday: registerForm['birthdate'].value,
-      gender: registerForm['gender'].value,
-      testSpeeds: 1,
-      type: "user"
-    });
-  }).then(() => {
-    /* var user = firebase.auth().currentUser;
-    user.sendEmailVerification() */;
-    //console.log(user.uid);
-    firebase.auth().onAuthStateChanged(user => {
-      if (user) {
-        id = user.uid;
-        console.log(id);
-        db.collection("TestResults").doc(id).set({
-          exists: true
-        }).then(() => { sendemail(); });
-      }
-    });
-    //window.location = 'index.html';
-  }).catch(error => {
-    registerForm.querySelector('.error').textContent = error.message;
-  });
 
 });
 
