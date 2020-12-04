ff862e81c5b3ad9de5d6f0df0756936f958c7204
JHuraibi <fh1328@wayne.edu>
2020-12-04 00:02:05 -0500

    Comment remove

--- public/physicians/patientDropdownList.js
+++ public/physicians/patientDropdownList.js
@@ -28,21 +28,6 @@ let patients = [];
 				});
 		}
 	});
-
-/* 	let patient1 = {
-		id: "Ii4NKqGowiVtH14FK8O3b7Wfuri1",
-		firstName: "John",
-		lastName: "Doe",
-	}
-
-	let patient2 = {
-		id: "NqP2JTgoCpYPixkCFVkvFyHUs7x1",
-		firstName: "Smitty",
-		lastName: "WerbenJagermanJensen",
-	}
-	console.log("PUSHING USERS");
-	patients.push(patient1);
-	patients.push(patient2); */
 }());
 
 
