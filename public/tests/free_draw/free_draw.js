67b2b227fe49a8e56740d1ef9d371e82f0249d76
JHuraibi <fh1328@wayne.edu>
2020-12-06 18:46:18 -0500

    Update Free Draw docstrings

--- public/tests/free_draw/free_draw.js
+++ public/tests/free_draw/free_draw.js
@@ -20,7 +20,7 @@ let actions = [];
 let actionCounter;
 
 /**
- * Sets up canvas, records the current time, sets up brush size slider reference,
+ * Sets up a new canvas, records the current time, sets up brush size slider DOM reference,
  * 	records slider's current value, and sets up upload and exit button group references.
  */
 function setup() {
@@ -39,8 +39,16 @@ function setup() {
 
 /**
  * Main test controller. Automatically loops 60 times per second.
+ *
+ * The variable clickDelay:
+ * 	clickDelay is used to allow for instances where a browser will pause recording
+ * 	of a mouse position. Such as when a user right clicks, moves the mouse, and then
+ * 	closes the right click context menu. This would register as the mouse cursor moving
+ * 	from the location of the right click to the location at the time of canceling the
+ * 	right click menu, all in the time of a single frame.
+ *	~ ~ ~
  * If the brush is active:
- * 	Draws a new line from the previous mouse cursor location
+ * 	Draws a new line segment from the previous mouse cursor location
  * 	to the current mouse cursor location. Sends the coordinates
  * 	of the new line and brush size of the line to saveLine().
  * Always:
@@ -61,7 +69,6 @@ function draw() {
 			let segment = drawing[i];
 			strokeWeight(segment.w);
 			line(segment.x, segment.y, segment.pX, segment.pY);
-			
 		}
 	}
 	
@@ -73,15 +80,15 @@ function draw() {
 }
 
 /**
- * Saves the attributes of the most-recently drawn line. The two coordinate
- * 	points ((x, y) and (pX, pY)) are the start and end points of a line
+ * Saves the attributes of the most-recently drawn line as a JSON. The two
+ * 	coordinate points ((x, y) and (pX, pY)) are the start and end points of a line
  * 	segment. W is simply the thickness of the line segment (value is set
  * 	by the slider). The end point of the previously drawn line becomes
  * 	the start point for the current line.
  * @param x		X-coordinate of the end point line
  * @param y     Y-coordinate of the end point line
- * @param pX	X-coordinate of the PREVIOUSLY drawn line end point, start point for new line
- * @param pY	Y-coordinate of the PREVIOUSLY drawn line end point, start point for new line
+ * @param pX	X-coordinate of the PREVIOUSLY drawn line end point (now is the start point for new line)
+ * @param pY	Y-coordinate of the PREVIOUSLY drawn line end point (now is the start point for new line)
  * @param w		Width (stroke weight) of the line
  */
 function saveLine(x, y, pX, pY, w) {
@@ -97,9 +104,9 @@ function saveLine(x, y, pX, pY, w) {
 }
 
 /**
- * Handles mouse down press events. If the cursor was within the bounds
- * 	of the canvas when the down press was registered, sets the brush to
- * 	active. If canvas was empty, updates the webpage's buttons by calling
+ * Handles mouse down press events. If the click was NOT a left, function returns.
+ * If the cursor was within the bounds of the canvas when the down press was registered,
+ *  sets the brush to active. If canvas was empty, updates the webpage's buttons by calling
  * 	enableUpload().
  */
 function mousePressed() {
@@ -130,7 +137,9 @@ function mousePressed() {
 }
 
 /**
- * Handles mouse up release events. Disables the brush.
+ * Handles mouse up release events. Disables the brush and pushes
+ * 	the the number of actions (i.e. individual line segments) that
+ * 	was recorded since the last left mouse down event.
  */
 function mouseReleased() {
 	if (leftClicked) {
@@ -158,29 +167,29 @@ function checkCursorBounds() {
 	}
 }
 
+/**
+ * Handles keypress combinations of Ctrl+Z for Undo and Ctrl+Y for Redo.
+ * NOTE: The return value is not used directly in this program.
+ * 		(For more info: https://p5js.org/reference/#/p5/keyPressed )
+ * @returns {boolean}
+ */
 function keyPressed() {
+	// ASCII:
 	// 89  === 'y'
 	// 121 === 'Y'
 	// 90  === 'z'
 	// 122 === 'Z'
-	if (keyIsDown(CONTROL) && (keyIsDown(90) || keyIsDown(122))) {
-		let removalIndex = drawing.length - actions.pop();
-		
-		recordLast(removalIndex);
-		
-		drawing.splice(removalIndex, drawing.length);
-		clear();
-		background(backgroundColor);
-	}
-	else if (keyIsDown(CONTROL) && (keyIsDown(89) || keyIsDown(121))) {
-		redo();
-	}
 	
-	return false; // prevent any default behaviour (P5.js reference recommendation)
+	if 		(keyIsDown(CONTROL) && (keyIsDown(90) || keyIsDown(122))) { undo(); }
+	else if (keyIsDown(CONTROL) && (keyIsDown(89) || keyIsDown(121))) { redo(); }
+	
+	return false; // prevent any default behaviour (Recommended by P5.js reference)
 }
 
 /**
- * Same functionality as pressing CTRL+Z (which is handled by keyPressed())
+ * Gets the most recent integer number of actions (See mouseReleased() ) and
+ * 	removes that many of items from drawing[]. This effectively removes those
+ * 	drawings by erasing their existence from record.
  */
 function undo() {
 	let removalIndex = drawing.length - actions.pop();
@@ -192,13 +201,23 @@ function undo() {
 	background(backgroundColor);
 }
 
+/**
+ * Is called by undo(). Takes the number of items to remove (index: int)
+ * 	from drawing[] and pushes each to a temporary storage array. This is to
+ * 	reverse the action of a undo by putting the removed drawings back into
+ * 	drawing[].
+ * @param index
+ */
 function recordLast(index) {
 	redoRecord = [];
 	for (let i = index; i < drawing.length; i++) {
 		redoRecord.push(drawing[i]);
 	}
 }
-
+/**
+ * Takes the drawings removed by the most-recent Undo and pushes it back
+ * 	into drawing[].
+ */
 function redo() {
 	// TODO: Needs a prelim check before pushing to drawing[]
 	for (let i = 0; i < redoRecord.length; i++) {
@@ -246,7 +265,7 @@ function drawStaticAxes() {
 }
 
 /**
- * Draws the static border around the canvas.
+ * Draws static border around the canvas.
  */
 function drawStaticBorder() {
 	noFill();
@@ -258,9 +277,9 @@ function drawStaticBorder() {
 /**
  * Clears any drawings on the canvas. Empties out drawings[], which is the
  * 	array that is storing the information of each line being drawn.
+ * NOTE: The corresponding HTML button element is in the free_draw.html file.
  */
 function clearCanvas() {
-	// NOTE: Button HTML element is declared in free_draw.html
 	drawing.splice(0, drawing.length);
 	clear();
 	background(backgroundColor);
@@ -272,7 +291,8 @@ function clearCanvas() {
 /**
  * Swaps the greyscale colors of the canvas background and the brush color.
  * Since the background is not being updated during draw(), background() must be called at
- *  the end of this function to explicitly draw the new background color to the canvas.
+ *  the end of this function to explicitly draw the new background with the new color to
+ *  the canvas.
  */
 function invertColors() {
 	let clicheTemp = backgroundColor;
