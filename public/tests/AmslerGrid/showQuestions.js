7b74bff5d252593d53363d0a1ef46cf208229b02
JHuraibi <fh1328@wayne.edu>
2020-12-09 00:15:48 -0500

    Testing Writing to Answers

--- public/tests/AmslerGrid/showQuestions.js
+++ public/tests/AmslerGrid/showQuestions.js
@@ -1,45 +1,44 @@
-var db = firebase.firestore();
+// var db = firebase.firestore();
+
+let testResult;
 
 
+async function loadAll(userRef) {
+	await db
+		.collection("TestResults")
+		.doc(userRef.uid)
+		.collection("AmslerGrid")
+		.orderBy("TimeStampMS", "desc")
+		.limit(1)
+		.get()
+		.then((querySnapshot) => {
+			querySnapshot.forEach((doc) => {
+				testResult = doc.data();
+			});
+		});
+	answerToQOne();
+	// this.manualAdd();
+};
 
-async function loadAll() {
-    await this.dbRef
-        .collection("TestResults")
-        .doc(this.userID)
-        .collection("AmslerGrid")
-        .orderBy("TimeStampMS", "desc")
-        .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                let extractedDoc = this.extractor(doc.id, doc.data());
-                this.docList.push(extractedDoc);
-            });
-        });
-
-    // this.manualAdd();
+function insert(data) {
+	// doc.data().Section1.concerns;
+	document.getElementById('q1').placeholder = data.AmslerQuestions.q1.value;
+	document.getElementById('sq1').placeholder = data.sec1Answers;
+	document.getElementById('sq2').placeholder = data.sec2Answers;
+	document.getElementById('sq3').placeholder = data.sec3Answers;
+	document.getElementById('sq4').placeholder = data.sec4Answers;
+	
+	
 }
 
-function extractor(id, data) {
-    return {
-        id: id,
-        TestName: data.TestName,
-        TimeStampMS: data.TimeStampMS,
-        Eye: data.AmslerQuestions.q1.value,
-        Section1: data.sec1Answers,
-        Section2: data.sec2Answers,
-        Section3: data.sec3Answers,
-        Section4: data.sec4Answers,
-    }
-
+function answerToQOne() {
+	console.log("hi");
+	
+	if (testResult.Section1) {
+		document.getElementById("testQ").innerHTML = "Yes";
+	}
+	else {
+		document.getElementById("testQ").innerHTML = "No";
+	}
 }
 
-insert(data)
-{
-    document.getElementById('q1').placeholder = data.AmslerQuestions.q1.value;
-    document.getElementById('sq1').placeholder = data.sec1Answers;
-    document.getElementById('sq2').placeholder = data.sec2Answers;
-    document.getElementById('sq3').placeholder = data.sec3Answers;
-    document.getElementById('sq4').placeholder = data.sec4Answers;
-    
-
-}
